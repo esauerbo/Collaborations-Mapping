b866476da725925536cc4dcd576d193eb579cb33
esauerbo <esauerbo@calpoly.edu>
2020-11-10 11:55:27 -0800

    added outgraph

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+const CALPOLYLATLNG = {lat: 37, lng: -115.65};
+const ZOOM = 14;
 
+/*const mapStyles = {
+  center: CALPOLYLATLNG,
+  zoom: ZOOM,
+  mapTypeControl: false
+};
+*/
 const mapStyles = {
   width: '100%',
-  height: '100%'
+  height: '100%',
 };
 
 export class MapContainer extends Component {
   render() {
     return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={
-          {
-            lat: -1.2884,
-            lng: 36.8233
-          }
-        }
-      />
+      <div className="map">
+        <Map
+          google={this.props.google}
+          zoom={6}
+          style={mapStyles}
+          initialCenter={CALPOLYLATLNG}
+        />
+      </div>
     );
   }
 }
@@ -27,3 +32,4 @@ export class MapContainer extends Component {
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyC1fm5j1RQqJkszE5Z0KSyACHdX8vsC-mo'
 })(MapContainer);
+
